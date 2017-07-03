53924362768dd9a894b7b5b71f36d1f4dcf60990
LAJordanger <lars.jordanger@gmail.com>
2017-07-03 15:22:52 +0200

    A minor test of the file hierarchy...

---
+++ inst/scRipts/paper1/file1_test.R
@@ -0,0 +1,10 @@
+##  This is only a test.
+
+##  Will a reference to a package not present be a problem in the testing algorithm...
+
+require(NonExistingPackage)
+
+3 + 4 != 6
+
+
+print("this is just a test...")
